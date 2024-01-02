22441e5c32b58881a8d683344bc5b2c405248ee3
muhammadyahyo <74825893+muhammadyahyo@users.noreply.github.com>
2024-01-02 11:47:00 +0500

    Nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "nprogress/nprogress.css"
 import type { AppProps } from 'next/app'
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import createEmotionCache from 'src/helpers/create-emotion-cache';
@@ -6,6 +7,9 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from 'src/helpers/theme';
 import { CssBaseline } from '@mui/material';
+import NProgress from 'nprogress';
+import { useEffect } from "react";
+import Router from 'next/router';
 
 
 const clientSideEmotionCache = createEmotionCache()
@@ -16,6 +20,22 @@ export interface MyAppProps extends AppProps{
 
 function MyApp(props: MyAppProps) {
   const {Component, emotionCache = clientSideEmotionCache, pageProps} = props
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+    
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+ 
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
   return (
     <CacheProvider value={emotionCache}>
       <Head>
